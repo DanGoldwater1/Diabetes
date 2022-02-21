8472b8a28e7bd3a3a89cadfc7e5469f07a554064
DanGoldwater1 <danielgoldwater1@nhs.net>
2022-02-21 17:16:00 +0000

    Added comments

--- Diabetes_Pipeline.py
+++ Diabetes_Pipeline.py
@@ -4,33 +4,29 @@ import gptables as gpt
 from numpy import outer
 import pandas as pd
 from pathlib import Path
+from datetime import date
+
+
 
 
 #First we set some parameters:
-source_data = 'Book1.csv'
+source_data = 'Diabetes_Source.csv'
 output_file_name = 'Diabetes_data_gptables.xlsx'
+parent_dir = Path(__file__).parent
+
+#Here we select our theme. This is a simple file and easy to edit, if we want to change the appearance of our Excel file. For example, I have set the title font color to blue. 
 theme_to_use = 'diabetes_theme.yml'
 
+
+#Next we want to import the frontmatter which will be presented on the first sheet of the excel book. This is a lot of text, so we've stored it in 'about.txt'
 with open('about.txt', 'r') as file:
     about = file.read()
-    about = about.splitlines()
+    about = about.splitlines() #This helps with the formatting
 
-# about = ['OK']
-print('foo')
-
-
-# about = ['some',
-#     {'bold': True},
-#     'This is the text'
-# ]
-
-# from info import about 
-
-parent_dir = Path(__file__).parent
 
 #Now we import our CSV data
 diabetes_df = pd.read_csv(source_data)
-diabetes_df.head()
+
 
 
 ######### PROCESSING BLOCK
@@ -40,17 +36,15 @@ diabetes_df.head()
 # In our output Excel file, we will want two sheets; one for data pertaining to type 1 diabetes, and one for type 2. So let's go ahead and create those. 
 
 diabetes_type_1_df = diabetes_df.loc[diabetes_df['Type']==1]
-diabetes_type_1_df = diabetes_type_1_df.drop(columns='Type')
+diabetes_type_1_df = diabetes_type_1_df.drop(columns='Type') #We don't need the info about diabetes type anymore
 
 diabetes_type_2_df = diabetes_df.loc[diabetes_df['Type']==2]
 diabetes_type_2_df = diabetes_type_2_df.drop(columns='Type')
 
 
-# Now we create the metadata for each of our sheets
-
+# Now we create the metadata for each of our sheets. We'll use a dictionary for this.
 type_1_kwargs = {
     "table_name": 'Type_1_Table_Data',
-    # 'title':["Mean", {"italic": True}, " Iris", "$$note2$$ sepal dimensions"],
     "title": 'Diabetes Data',
     "subtitles": ['Type 1 Diabetes',],
     "units": None,
@@ -76,20 +70,19 @@ type_2_kwargs.update({
 
 #Next, gptables needs to convert our data from a pandas format to a format excel is happy with. 
 diabetes_type_1_df = diabetes_type_1_df.reset_index()
-diabetes_type_1_df = diabetes_type_1_df.drop(columns='index')
-
 diabetes_type_1_table = gpt.GPTable(table=diabetes_type_1_df, **type_1_kwargs)
 diabetes_type_2_df = diabetes_type_2_df.reset_index()
 diabetes_type_2_table = gpt.GPTable(table=diabetes_type_2_df, **type_2_kwargs)
 
 ##### COVER
+#Here we create the cover sheet for the excel file. 
 cover = gpt.Cover(
     cover_label="Notes",
-    title="A Worbook containing good practice tables",
-    intro=["This is some introductory information", "And some more"],
-    about=about,
-    contact=["John Doe", "Tel: 345345345"],
-    additional_elements=["subtitles", "scope", "source", "notes"]
+    title="National Diabetes Audit (NDA) 2021-22 quarterly report for England, Clinical Commissioning Groups and GP practices - PROVISIONAL",
+    intro=[f"Publication Date: {date.today()}", ], #This appears as a sub-heading
+    about=about, #This is where we pull in all the information from our text file. 
+    contact=["Author Name", "Tel: -----"],
+    additional_elements=["subtitles", "scope", "source", "notes"] 
     )
 
 
@@ -102,12 +95,13 @@ if __name__=="__main__":
 
         filename=output_path,
         sheets={
-            "Type 1 Data": diabetes_type_1_table, #To the left of the colon sets the sheet name
+            #To the left of the colon sets the sheet name. To the right of the colon sets the sheet content. Here we are assigning the tables we created above - not the pandas dataframes!
+            "Type 1 Data": diabetes_type_1_table, 
             "Type 2 Data": diabetes_type_2_table
         },
         cover=cover,
         auto_width=True,
-        theme=gpt.Theme(theme_to_use)
+        theme=gpt.Theme(theme_to_use) #Selecting our theme from the top
     )
     print(f'Output written to {output_file_name}')
 
