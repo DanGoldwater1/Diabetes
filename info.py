a1627a5dd1ebd53ea403437c87f6f60b89a80f8a
DanGoldwater1 <danielgoldwater1@nhs.net>
2022-02-22 09:13:00 +0000

    removed unnecessary files

--- info.py
+++
@@ -1,6 +0,0 @@
-# about = [
-#     # {'bold': True},
-#     'This report provides provisional England, Clinical Commissioning Group (CCG) and GP practice level data for January to September 2021 (part of the NDA 2021-22 audit period). ', 
-#     'The worksheets include data for all CCGs and GP practices in England within the November 2021 extraction of NDA data (January 2021 to September 2021 data). These data are provisional and have been subject to fewer data quality and validation checks than data included in final NDA publications. Data for Local Health Boards (LHB) in Wales and Specialist Diabetes Services are not included in this publication. Their 2021-22 data (for the full audit period January 2021 to March 2022) will be extracted as part of the final NDA 2021-22 collection and reported in the NDA 2021-22 short report publication, scheduled for late 2022.'
-#     ]
-
